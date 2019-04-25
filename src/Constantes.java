a022f17ac10498ea8ff3a8cc84f5e18877fe5e5b
FedesMM <fmatonte@gmail.com>
2019-04-25 19:04:00 -0300

    Arranca la tesisgit commit -m!

---
+++ src/Constantes.java
@@ -0,0 +1,87 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Constantes {
+    //Luego pasar a globales
+
+    public static int cantPixeles = 18475;
+    public static int cantPotreros = 26168;
+    /**/
+    /*
+    public static int cantPixeles = 792;
+    public static int cantPotreros = 1602;
+    */
+    public static int cantEstaciones = 16;
+    public static int cantAnios = cantEstaciones/4;
+    public static int cantUsos= 15; //El cero esta reservado para inicio random
+    public static int cantProductores= 42;
+    public static int minimaCantidadUsos=3;
+    public static int maximaCantidadUsos=6;
+
+    //Maximo errores posibles para la funcion objetivo valor objetivo
+    public static float maximoIncumplimientoFosforo=2.23F*cantAnios;
+    public static float maximoIncumplimientoProductividadMinimaEstacion= 6000;
+    public static float maximoIncumplimientoUsosDistintos=Constantes.cantEstaciones*Constantes.cantProductores;
+    public static float pesoIncumplimientoFosforo=1;
+    public static float pesoIncumplimientoProductividadMinimaEstacion= 1;
+    public static float pesoIncumplimientoUsosDistintos=1;
+
+    public static float mpp=1.0f;
+    public static float[]  restriccionProductividadProductorE = new float[]
+            {mpp*1200,mpp*600,mpp*2100,mpp*2100,mpp*1200,mpp*600,mpp*2100,mpp*2100,mpp*1200,mpp*600,mpp*2100,mpp*2100,mpp*1200,mpp*600,mpp*2100,mpp*2100};
+
+
+
+
+    /*
+    //Constantes de test
+    public static final int cantPixeles = 10;
+    public static final int cantEstaciones = 12;
+    public static final int cantAnios = cantEstaciones/4;
+    public static final int cantUsos= 5;
+    public static final int cantProductores= 2;
+    public static int minimaCantidadUsos=2;
+    public static int maximaCantidadUsos=4;
+    */
+
+
+    public static Uso[] usos = new Uso[cantUsos]; //el cero queda reservado para el no uso y el 14 cuando no se toca desde la ultima ves
+    public static Pixel[] pixeles = new Pixel[cantPixeles];
+    public static Productor[] productores= new Productor [cantProductores];
+    public static int semilla=0;
+    public static Random uniforme = new Random();
+    public static List<Integer> productoresActivos= new ArrayList<Integer>();
+
+    public static void cargarInstancia() {
+        String nombreInstancia = "./Instancias/IntanciaPrueba.in";
+        cantPixeles = Pixel.contarLineas(nombreInstancia);
+        System.out.println(nombreInstancia + "\tCantPixeles:" + cantPixeles);
+        Constantes.cantPixeles = cantPixeles;
+        Constantes.cantPotreros = cantPixeles;
+        Constantes.productores = Productor.cargarProductores();
+        Constantes.pixeles = Pixel.cargarPixeles(nombreInstancia);
+        Constantes.usos = Uso.cargarUsos();
+        Constantes.maximoIncumplimientoUsosDistintos = Constantes.cantEstaciones * Constantes.productoresActivos.size();
+    }
+
+    public static void cargarInstanciaProductor( int numProductor) {
+        String nombreInstancia = "./Instancias/IntanciaPrueba.in";
+        cantPixeles = Pixel.contarLineas(nombreInstancia);
+        System.out.println(nombreInstancia + "\tCantPixeles:" + cantPixeles);
+        Constantes.cantPixeles = cantPixeles;
+        Constantes.cantPotreros = cantPixeles;
+        Constantes.productores = Productor.cargarProductores();
+        Constantes.pixeles = Pixel.cargarPixelesDeProductor(nombreInstancia, numProductor);
+        Constantes.usos = Uso.cargarUsos();
+        Constantes.maximoIncumplimientoUsosDistintos = Constantes.cantEstaciones * Constantes.productoresActivos.size();
+    }
+
+
+
+
+}
+
+
+
+
---
+++ src/Productor.java
@@ -0,0 +1,90 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static java.lang.Integer.min;
+
+public class Productor {
+    int numeroProductor;
+    float[] restriccionProduccionEstacion;
+    List<Integer> pixelesDelProductor;
+    float areaTotal;
+    public int getMinCantUsos(){
+        return min(this.pixelesDelProductor.size(), Constantes.minimaCantidadUsos);
+    }
+
+    public Productor(int numeroProductor, float[] restriccionProduccionEstacion, float[] restriccionProduccionAnual, List<Integer> pixelesDelProductor) {
+        this.numeroProductor = numeroProductor;
+        this.restriccionProduccionEstacion = restriccionProduccionEstacion;
+        this.pixelesDelProductor = pixelesDelProductor;
+        this.areaTotal=0;
+    }
+
+    public static Productor[] cargarProductores(){
+        float[] restriccionProductorE, restriccionProductorA;
+        List<Integer> pixelesDelProductor;
+        Productor[] productores = new Productor[Constantes.cantProductores];
+        //El productor cero tiene todos los pixeles pares
+        restriccionProductorE= new float[] {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+        restriccionProductorA= new float[] {0,0,0,0};
+        pixelesDelProductor= new ArrayList<Integer>();
+
+
+        //PRUEBA SIN RESTRICCIONES PARA EL PRODUCTOR 1
+        productores[0]= new Productor(0,restriccionProductorE, restriccionProductorA,pixelesDelProductor );
+        for (int iProductores = 1; iProductores < Constantes.cantProductores; iProductores++) {
+            //El productor cero tiene todos los pixeles pares
+
+            restriccionProductorA= new float[] {6000,6000,6000,6000};
+            pixelesDelProductor= new ArrayList<Integer>();
+            productores[iProductores]= new Productor(iProductores,Constantes.restriccionProductividadProductorE, restriccionProductorA,pixelesDelProductor );
+        }
+
+        return productores;
+    }
+
+
+
+    public static Productor[] cargarProductoresTest(){
+        Productor[] productores = new Productor[Constantes.cantProductores];
+
+        //El productor cero tiene todos los pixeles pares
+        float[] restriccionProductorE0= new float[] {1200,600,2100,2100,1200,600,2100,2100,1200,600,2100,2100};
+        float[] restriccionProductorA0= new float[] {6000,6000,6000};
+        productores[0]= new Productor(0,restriccionProductorE0, restriccionProductorA0,Arrays.asList(2,4));
+        //El productor uno tiene todos los pixeles inpares
+        float[] restriccionProductorE1= new float[] {1200,600,2100,2100,1200,600,2100,2100,1200,600,2100,2100};
+        float[] restriccionProductorA1= new float[] {6000,6000,6000};
+        productores[1]= new Productor(0,restriccionProductorE1, restriccionProductorA1,Arrays.asList(1,3));
+        return productores;
+    }
+    public void imprimirProductor(){
+        System.out.printf("("+this.numeroProductor+", "+this.areaTotal+", {");
+
+        for (int i = 0; i <this.restriccionProduccionEstacion.length ; i++) {
+            System.out.print(this.restriccionProduccionEstacion[i]);
+            if (i!=(this.restriccionProduccionEstacion.length-1)){
+                System.out.printf(",");
+            }
+        }
+        System.out.printf("}, {");
+        for (int i = 0; i <this.pixelesDelProductor.size(); i++) {
+            System.out.print(this.pixelesDelProductor.get(i));
+            if (i!=(this.pixelesDelProductor.size()-1)) {
+                System.out.printf(",");
+            }
+        }
+        System.out.println("})");
+    }
+    public static void imprimirProductores(){
+        for (int i = 0; i < Constantes.productores.length; i++) {
+            Constantes.productores[i].imprimirProductor();
+        }
+    }
+    public static void imprimirProductoresActivos(){
+        for (int i = 0; i < Constantes.productoresActivos.size(); i++) {
+            Constantes.productores[Constantes.productoresActivos.get(i)].imprimirProductor();
+        }
+    }
+
+}
